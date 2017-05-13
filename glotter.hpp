a686cdea3af88ae18a5867de2813ffde5471a4ad
Mateusz Cegiełka <mateusz@cegla.net>
2017-05-13 11:52:29 +0200

    switched to uWebSockets

--- glotter.hpp
+++ glotter.hpp
@@ -1,46 +1,46 @@
 #pragma once
 
 #include <bits/stdc++.h>
-#include <seasocks/PrintfLogger.h>
-#include <seasocks/Server.h>
-#include <seasocks/WebSocket.h>
+#include <uWS/uWS.h>
 
 class Glotter {
 
 public:
 
-    Glotter():
-        server(std::make_shared<seasocks::PrintfLogger>(seasocks::Logger::Level::INFO))
-    {
-        server.addWebSocketHandler("/ws", std::make_shared<HandlerWS>(this));
-        server.serve(".", 57077);
+    Glotter(){
+        uws.onHttpRequest([&](uWS::HttpResponse* res, uWS::HttpRequest req, char* data, size_t len, size_t remain){
+            std::clog << "'" << req.getUrl().toString() << "'" << std::endl;
+            if (req.getMethod() == uWS::METHOD_GET) {
+                if (req.getUrl().toString() == "/") {
+                    auto html = readFile("index.html");
+                    res->end(html.c_str(), html.size());
+                } else if (req.getUrl().toString() == "/style.css") {
+                    auto css = readFile("style.css");
+                    res->end(css.c_str(), css.size());
+                } else if (req.getUrl().toString() == "/glotter.js") {
+                    auto js = readFile("glotter.js");
+                    res->end(js.c_str(), js.size());
+                }
+            }
+        });
+        uws.listen(57077);
+        uws.run();
     }
 
-    void addEdge(int a, int b) { conn->send("addEdge " + std::to_string(a) + " " + std::to_string(b)); }
-    void resize(int n) { conn->send("resize " + std::to_string(n)); }
+    void addEdge(int a, int b){}
+    void resize(int n){}
 
 private:
 
-    struct HandlerWS : public seasocks::WebSocket::Handler {
+    static std::string readFile(const std::string& path) {
+        std::ifstream file;
+        file.exceptions(std::ios::badbit | std::ios::failbit);
+        file.open(path);
+        std::string buf;
+        std::getline(file, buf, '\0');
+        return buf;
+    }
 
-        Glotter* glotter;
-        HandlerWS(Glotter* glotter):glotter(glotter){}
-
-        void onConnect(seasocks::WebSocket* evconn) override {
-            if (!glotter->conn) glotter->conn = evconn;
-            // TODO: handle error
-        }
-        void onDisconnect(seasocks::WebSocket*) override {
-            // TODO: handle disconnect
-        }
-        void onData(seasocks::WebSocket*, const char*) override {
-            // TODO: handle data
-        }
-
-    };
-    friend HandlerWS;
-
-    seasocks::Server server;
-    seasocks::WebSocket* conn = nullptr;
+    uWS::Hub uws;
 
 };

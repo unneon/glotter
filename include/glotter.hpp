29f61b4087422cff1060ebf1762e21bc1a738b40
Mateusz Cegiełka <mateusz@cegla.net>
2017-06-16 10:08:02 +0200

    proper logging

--- include/glotter.hpp
+++ include/glotter.hpp
@@ -7,10 +7,21 @@ class Glotter {
 
 public:
 
+	enum class LogLevel { nothing = 0, important = 1, debug = 2 };
+	friend bool operator>=(LogLevel a, LogLevel b) { return static_cast<int>(a) >= static_cast<int>(b); }
+
     Glotter(){
+		if (auto reqLL = getenv("GLOTTER_LOGLEVEL")) {
+			using namespace std::string_literals;
+			if (reqLL == "nothing"s) ll = LogLevel::nothing;
+			else if (reqLL == "important"s) ll = LogLevel::important;
+			else if (reqLL == "debug"s) ll = LogLevel::debug;
+			else log(LogLevel::important, [&]{ std::clog << "Unknown log level\"" << reqLL << "\""; });
+		}
+
         uws.onHttpRequest([&](uWS::HttpResponse* res, uWS::HttpRequest req, char* /*data*/, size_t /*len*/, size_t /*remain*/){
-            std::cout << (req.getMethod() == uWS::METHOD_GET ? "GET " : "(unknown method) ") << req.getUrl().toString() << std::endl;
-            if (req.getMethod() == uWS::METHOD_GET) {
+            log(LogLevel::debug, [&]{ std::clog << (req.getMethod() == uWS::METHOD_GET ? "GET " : "(unknown method) ") << req.getUrl().toString(); });
+			if (req.getMethod() == uWS::METHOD_GET) {
                 auto sharefile = [&](const char* alias, const char* name){
                     if (req.getUrl().toString() == alias) {
                         auto r = readFile(name);
@@ -23,14 +34,16 @@ public:
 				sharefile("/bebas.ttf", "web/BEBAS___.ttf");
             }
         });
-        uws.onConnection([&](uWS::WebSocket<uWS::SERVER>* conn, auto){
-            std::cout << "Connected on WebSocket" << std::endl;
+        uws.onConnection([&, this](uWS::WebSocket<uWS::SERVER>* conn, auto){
+            this->log(LogLevel::important, []{ std::clog << "Connected"; });
             ws = conn;
         });
-		auto port = 57077;
+		uws.onDisconnection([&, this](uWS::WebSocket<uWS::SERVER>*, int, char*, size_t){
+			this->log(LogLevel::important, []{ std::clog << "Disconnected"; });
+		});
         uws.listen(port);
         std::thread([&]{uws.run();}).detach();
-		std::cout << "Listening on port " << port << std::endl;
+		log(LogLevel::debug, [&]{ std::clog << "Listening on port " << port; });
     }
 
     void addEdge1(int a, int b){
@@ -51,6 +64,8 @@ public:
     void resize(int n){
         send("resize " + std::to_string(n));
     }
+	void setLogLevel(LogLevel newll) { ll = newll; }
+	template <typename F> void log(LogLevel priority, F f) { if (ll >= priority) { std::clog << "[GLOTTER] "; f(); std::clog << std::endl; } }
 
 private:
 
@@ -64,16 +79,18 @@ private:
     }
     void send(const std::string& s) {
         if (!ws) {
-            std::cout << "Waiting for connection..." << std::endl;
+			log(LogLevel::important, [&]{ std::clog << "Waiting for connection at port " << port; });
             while (!ws)
                 std::this_thread::sleep_for(std::chrono::milliseconds(50));
         }
-		std::clog << s << std::endl;
+		log(LogLevel::debug, [&]{ std::clog << s; });
         ws->send(s.c_str(), s.size(), uWS::OpCode::TEXT);
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
     uWS::Hub uws;
     uWS::WebSocket<uWS::SERVER>* ws = nullptr;
+	LogLevel ll = LogLevel::important;
+	unsigned short port = 57077;
 
 };
